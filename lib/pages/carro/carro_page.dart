2e3f8f80eb7de058a879a569fd726b7886a4a154
chico <fcn_jr@hotmail.com>
2021-03-15 21:52:46 -0300

    Aula 140 - Layout2 carro

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -51,42 +51,48 @@ class CarroPage extends StatelessWidget {
       child: ListView(
         children: <Widget>[
           Image.network(carro.urlFoto),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  text(carro.nome, fontSize: 20, bold: true),
-                  text(
-                    carro.tipo,
-                    fontSize: 16,
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                      size: 40,
-                    ),
-                    onPressed: _onClickFavorite,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.share,
-                      size: 40,
-                    ),
-                    onPressed: _onClickShare,
-                  ),
-                ],
-              )
-            ],
-          ),
+          _bloco1(),
+          Divider(),
+          _bloco2(),
         ],
       ),
+    );
+  }
+
+  _bloco1() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            text(carro.nome, fontSize: 20, bold: true),
+            text(
+              carro.tipo,
+              fontSize: 16,
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 40,
+              ),
+              onPressed: _onClickFavorite,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.share,
+                size: 40,
+              ),
+              onPressed: _onClickShare,
+            ),
+          ],
+        )
+      ],
     );
   }
 
@@ -111,4 +117,19 @@ class CarroPage extends StatelessWidget {
   void _onClickFavorite() {}
 
   void _onClickShare() {}
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        text(carro.descricao, fontSize: 16, bold: true),
+        SizedBox(
+          height: 20,
+        ),
+        text(
+            "Converters are often used with streams to transform the data that comes through the stream as it becomes available. The following code uses two converters. The first is a UTF-8 decoder, which converts the data from bytes to UTF-8 as it's read from a file, The second is an instance of LineSplitter, which splits the data on newline boundaries.Converters are often used with streams to transform the data that comes through the stream as it becomes available. The following code uses two converters. The first is a UTF-8 decoder, which converts the data from bytes to UTF-8 as it's read from a file, The second is an instance of LineSplitter, which splits the data on newline boundaries.Converters are often used with streams to transform the data that comes through the stream as it becomes available. The following code uses two converters. The first is a UTF-8 decoder, which converts the data from bytes to UTF-8 as it's read from a file, The second is an instance of LineSplitter, which splits the data on newline boundaries.Converters are often used with streams to transform the data that comes through the stream as it becomes available. The following code uses two converters. The first is a UTF-8 decoder, which converts the data from bytes to UTF-8 as it's read from a file, The second is an instance of LineSplitter, which splits the data on newline boundaries.Converters are often used with streams to transform the data that comes through the stream as it becomes available. The following code uses two converters. The first is a UTF-8 decoder, which converts the data from bytes to UTF-8 as it's read from a file, The second is an instance of LineSplitter, which splits the data on newline boundaries.",
+            fontSize: 16),
+      ],
+    );
+  }
 }
